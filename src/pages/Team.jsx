012a09a38affe1444c4da310864f84dd85a63731
Nandan Sharma <na.sha.delhi@gmail.com>
2022-12-01 17:45:17 +0530

    footer on team page

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -2,6 +2,7 @@ import './Team.css'
 import styles from '../style'
 import Navbar from '../components/Navbar'
 import TeamPage from '../components/TeamPage'
+import Footer from '../components/Footer'
 
 const Team = () => {
     return (
@@ -11,7 +12,12 @@ const Team = () => {
                     <Navbar />
                 </div>
             </div>
-            <TeamPage />
+            <div className={`bg-black ${styles.paddingX} ${styles.flexStart}`}>
+                <div className={`${styles.boxWidth}`}>
+                    <TeamPage />
+                    <Footer />
+                </div>
+            </div>
         </div>
     )
 }
