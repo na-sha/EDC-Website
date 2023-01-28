c36ddc4647132f5d8af3ac34dbfd9f87a6acc670
theabhiinavgarg <theabhinavgarg@outlook.com>
2023-01-28 23:07:34 +0530

    Update Deliverables.jsx

--- src/components/Deliverables.jsx
+++ src/components/Deliverables.jsx
@@ -16,6 +16,14 @@ const Deliverables = () => {
       <main className="grid-container flow">
         <div>
           <h1 className={"ff-serif fs-700 uppercase"}><span className='text-gradient'>Deliverables</span> by EDC</h1>
+          <ul>
+            <li>ACRYLIC ON THE BACKDROP WHEN ARTIST PERFORMS</li>
+            <li>GIVEAWAY OFFICIAL PRODUCTS</li>
+            <li>EXCLUSIVE SPONSOR VIDEO</li>
+            <li>SHOUTOUTS BY ARTIST</li>
+            <li>EVENT COVERAGE BY DRONE CAMERA</li>
+
+          </ul>
         </div>
         <div>
           <Proposal />
