d6464fa062bb868361973a221a0f9714d9d7bd6d
theabhiinavgarg <theabhinavgarg@outlook.com>
2023-01-28 20:19:43 +0530

    Update Proposal.jsx

--- src/components/Proposal.jsx
+++ src/components/Proposal.jsx
@@ -7,14 +7,14 @@ import { ArrowLeftCircleIcon, ArrowRightCircleIcon, } from '@heroicons/react/24/
 import './index.css';
 
 const handleClick = () => {
-  fetch('./prop.pdf').then(response => {
+  fetch('prop.pdf').then(response => {
     response.blob().then(blob => {
       // Creating new object of PDF file
       const fileURL = window.URL.createObjectURL(blob);
       // Setting various property values
       let alink = document.createElement('a');
       alink.href = fileURL;
-      alink.download = './prop.pdf';
+      alink.download = 'prop.pdf';
       alink.click();
     })
   })
